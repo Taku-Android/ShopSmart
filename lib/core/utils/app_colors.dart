304a1e3533cb6aa427edfd1c2fbe39ec632eaeeb
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-21 01:17:59 +0300

    adding all the app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:shop_smart/config/hex_color.dart';
+
+class AppColor{
+
+
+  static const Color lightScaffoldBackground = Colors.white ;
+  static  Color lightPrimary = HexColor('181059') ;
+  static const Color lightCardColor = Color.fromARGB(106, 250, 250, 250);
+  static const Color darkScaffoldBackground = Color.fromARGB(255 , 9, 3, 27) ;
+  static const Color darkPrimary = Color.fromARGB(255, 94, 75, 236) ;
+
+}
