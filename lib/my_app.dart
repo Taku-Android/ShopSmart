36199bfb0aac9591751345ab3330b1878bb01fee
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-21 04:23:56 +0300

    using blocBuilder  to build the widget when theme change

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_smart/config/app_routes.dart';
 import 'package:shop_smart/config/app_themes.dart';
+import 'package:shop_smart/core/utils/theme_cubit/theme_cubit.dart';
+import 'package:shop_smart/core/utils/theme_cubit/theme_state.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRoutes.router,
-      debugShowCheckedModeBanner: false,
-      theme: AppThemes.themeData(isDarkTheme: false, context: context),
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, state) {
+        return MaterialApp.router(
+          routerConfig: AppRoutes.router,
+          debugShowCheckedModeBanner: false,
+          theme: AppThemes.themeData(
+              isDarkTheme: BlocProvider
+                  .of<ThemeCubit>(context)
+                  .getIsDarkTheme,
+              context: context),
+        );
+
+      },
     );
   }
 }
