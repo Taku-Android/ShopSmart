b7acad6a97777591eef6d9f3902a36068abeeacb
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-21 06:04:14 +0300

    adding navigationBarTheme

--- lib/config/app_themes.dart
+++ lib/config/app_themes.dart
@@ -1,22 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:shop_smart/core/utils/app_colors.dart';
 
-class AppThemes{
-  static ThemeData themeData({required bool isDarkTheme , required BuildContext context}){
-    if(isDarkTheme){
+class AppThemes {
+  static ThemeData themeData(
+      {required bool isDarkTheme, required BuildContext context}) {
+    if (isDarkTheme) {
       return ThemeData(
-        scaffoldBackgroundColor: AppColor.darkScaffoldBackground ,
+        scaffoldBackgroundColor: AppColor.darkScaffoldBackground,
         cardColor: AppColor.darkCardColor,
-          brightness: Brightness.dark ,
+        brightness: Brightness.dark,
+        navigationBarTheme: NavigationBarThemeData(
+            elevation: 0,
+            backgroundColor: AppColor.darkScaffoldBackground,
+            labelTextStyle: MaterialStateProperty.resolveWith<TextStyle?>(
+                (Set<MaterialState> states) {
+              if (states.contains(MaterialState.selected)) {
+                return const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                );
+              } else {
+                return const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                );
+              }
+            })),
       );
-    }else{
+    } else {
       return ThemeData(
-          scaffoldBackgroundColor: AppColor.lightScaffoldBackground,
-          cardColor: AppColor.lightCardColor,
+        scaffoldBackgroundColor: AppColor.lightScaffoldBackground,
+        cardColor: AppColor.lightScaffoldBackground,
         // to change the text color
-        brightness: Brightness.light
-
-
+        brightness: Brightness.light,
+        navigationBarTheme: NavigationBarThemeData(
+            elevation: 0,
+            backgroundColor: AppColor.lightPrimary,
+            labelTextStyle: MaterialStateProperty.resolveWith<TextStyle?>(
+                (Set<MaterialState> states) {
+              if (states.contains(MaterialState.selected)) {
+                return const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                );
+              } else {
+                return const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                );
+              }
+            })),
       );
     }
   }
--- lib/features/home/persentation/screens/home_screen.dart
+++ lib/features/home/persentation/screens/home_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Home')
+        ],
+      ),
+    );
   }
 }
