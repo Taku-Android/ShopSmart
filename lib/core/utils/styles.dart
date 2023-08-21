3566e4701a0af0eca160160e6890fc096a79cdc6
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-21 06:03:41 +0300

    adding style for bottom navigation

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class Styles {
+
+  static  TextStyle? textBottomNavigationSelected = TextStyle(
+    fontSize: 16 ,
+    fontWeight: FontWeight.w600
+  );
+  static  TextStyle? textBottomNavigationNotSelected = TextStyle(
+      fontSize: 16 ,
+      fontWeight: FontWeight.w600
+  );
+
+}
