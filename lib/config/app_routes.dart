22f2b7e22ada8ddf8e6850c0e1c140f06d5dad64
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-21 06:01:31 +0300

    make the root dir is the start screen

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,15 +1,30 @@
 import 'package:go_router/go_router.dart';
+import 'package:shop_smart/features/root/persentation/screens/root_navigation_screen.dart';
 import '../features/home/persentation/screens/home_screen.dart';
 
 abstract class AppRoutes {
-  static const kHomeScreen = '/';
+  static const kRootScreen = '/';
+  static const kHomeScreen = '/home';
+  static const kCartScreen = '/cart';
+  static const kSearchScreen = '/search';
+  static const kProfileScreen = '/profile';
 
   static final router = GoRouter(routes: [
+
+    GoRoute(
+      path: kRootScreen,
+      builder: (context, GoRouterState state) {
+        return const RootNavigationScreen();
+      },
+    ),
     GoRoute(
       path: kHomeScreen,
       builder: (context, GoRouterState state) {
         return const HomeScreen();
       },
     ),
+
+
+
   ]);
 }
