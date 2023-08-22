8b860783c4063fdbb088f60802ed3255f314c1ec
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-22 05:26:45 +0300

    image_path.dart  - for all the assets paths to easily access the images

--- lib/core/utils/image_path.dart
+++ lib/core/utils/image_path.dart
@@ -1,8 +1,54 @@
 class ImagePath{
 
+  static String imagePath = 'assets/images';
+  static String bagPath = 'assets/images/bag';
+  static String bannersPath = 'assets/images/banners';
+  static String categoryPath = 'assets/images/categories';
+  static String profilePath = 'assets/images/profile';
 
-  String assetPath(String imageName){
-         return 'assets/images/$imageName.png';
-  }
+
+  // image path
+  static String address_map = '$imagePath/address_map.png';
+  static String empty_search = '$imagePath/empty_search.png';
+  static String error = '$imagePath/error.png';
+  static String forgot_password = '$imagePath/forgot_password.jpg';
+  static String rounded_map = '$imagePath/rounded_map.png';
+  static String successful = '$imagePath/successful.png';
+  static String warning = '$imagePath/warning.png';
+
+  //bag path
+  static String bag_wish = '$bagPath/bag_wish.png';
+  static String order = '$bagPath/order.png';
+  static String order_svg = '$bagPath/order_svg.png';
+  static String shopping_basket = '$bagPath/shopping_basket.jpg';
+  static String shopping_cart = '$bagPath/shopping_cart.png';
+  static String wishlist_svg = '$bagPath/wishlist_svg.png';
+
+  //banners
+  static String banner1 = '$bannersPath/banner1.png';
+  static String banner2 = '$bannersPath/banner2.png';
+
+  //categories
+  static String book_img = '$categoryPath/book_img.png';
+  static String cosmetics = '$categoryPath/cosmetics.png';
+  static String electronics = '$categoryPath/electronics.png';
+  static String fashion = '$categoryPath/fashion.png';
+  static String mobiles = '$categoryPath/mobiles.png';
+  static String pc = '$categoryPath/pc.png';
+  static String shoes = '$categoryPath/shoes.png';
+  static String watch = '$categoryPath/watch.png';
+
+
+  //profile
+  static String address = '$profilePath/address.png';
+  static String login = '$profilePath/login.png';
+  static String logout = '$profilePath/logout.png';
+  static String privacy = '$profilePath/privacy.png';
+  static String recent = '$profilePath/recent.png';
+  static String theme = '$profilePath/theme.png';
 
 }
+
+
+
+
