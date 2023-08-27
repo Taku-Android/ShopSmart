e458767070ee2c68f0f191c87ccad64bbcd027fe
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-27 04:15:58 +0300

    adding profile general list

---
+++ lib/features/profile/persentation/widgets/profile_general.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shop_smart/core/utils/image_path.dart';
+import 'package:shop_smart/core/utils/styles.dart';
+import 'package:shop_smart/features/profile/persentation/widgets/general_custom_list.dart';
+
+class ProfileGeneral extends StatelessWidget {
+  const ProfileGeneral({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'General',
+          style: Styles.text18,
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        GeneralCustomList(title: 'All Order', image: ImagePath.order_svg, onTap: (){
+
+        },),
+        GeneralCustomList(title: 'Wishlist', image: ImagePath.wishlist_svg, onTap: (){
+
+        },),
+        GeneralCustomList(title: 'Viewed recently', image: ImagePath.recent, onTap: (){
+
+        },),
+        GeneralCustomList(title: 'Address', image: ImagePath.address, onTap: (){
+
+        },),
+      ],
+    );
+  }
+}
