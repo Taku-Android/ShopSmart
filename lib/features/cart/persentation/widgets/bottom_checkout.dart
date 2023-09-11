428770a6c6f18a34c90f37e9e6ce383046fef5f0
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-09-11 04:30:18 +0300

    adding bottom_checkout.dart

---
+++ lib/features/cart/persentation/widgets/bottom_checkout.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shop_smart/core/utils/global_widgets/custom_button.dart';
+import 'package:shop_smart/core/utils/styles.dart';
+
+class BottomCheckout extends StatelessWidget {
+  const BottomCheckout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Total (6 Products / 6 Items)',
+                style: Styles.text18,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 8,),
+              Text(
+                '\$14000',
+                style: Styles.subTitleText.copyWith(
+                  color: Colors.lightBlueAccent
+                ),
+              ),
+            ],
+          ) ,
+          const Spacer(),
+          CustomButton(onPressed: (){}, title: 'Checkout')
+        ],
+      ),
+    );
+  }
+}
