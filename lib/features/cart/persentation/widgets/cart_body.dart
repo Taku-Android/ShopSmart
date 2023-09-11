cb0138a9cda8dea69b12db5a530471b43f1a3ee3
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-09-11 04:30:35 +0300

    adding the checkout to the cart_body.dart

--- lib/features/cart/persentation/widgets/cart_body.dart
+++ lib/features/cart/persentation/widgets/cart_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop_smart/core/utils/global_widgets/custom_appbar.dart';
 import 'package:shop_smart/core/utils/global_widgets/custom_empty_list.dart';
 import 'package:shop_smart/core/utils/image_path.dart';
+import 'package:shop_smart/features/cart/persentation/widgets/bottom_checkout.dart';
 import 'package:shop_smart/features/cart/persentation/widgets/custom_list_item.dart';
 
 class CartBody extends StatelessWidget {
@@ -20,6 +21,13 @@ class CartBody extends StatelessWidget {
         (isEmpty)
             ? CustomEmptyList(image: ImagePath.shopping_cart, page: 'cart')
             : const CustomListItem(),
+       const Spacer(),
+       const Divider(
+         height: 2,
+         color: Colors.grey,
+       ),
+       const SizedBox(height: 8,),
+       const BottomCheckout()
       ],
     );
   }
