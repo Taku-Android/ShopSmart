5d763b9cd956e719e4ce2c697c4f6e7e920dde7b
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-09-12 07:25:47 +0300

    finishing the search_body.dart

--- lib/features/search/persentation/widgets/search_body.dart
+++ lib/features/search/persentation/widgets/search_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_smart/features/search/persentation/widgets/search_item_grid_list.dart';
 import 'package:shop_smart/features/search/persentation/widgets/search_text_field.dart';
-
 import '../../../../core/utils/global_widgets/custom_appbar.dart';
 
 class SearchBody extends StatefulWidget {
@@ -28,6 +28,7 @@ class _SearchBodyState extends State<SearchBody> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const CustomAppbar(title: 'Store Product', needIcon: false),
         Padding(
@@ -44,7 +45,15 @@ class _SearchBodyState extends State<SearchBody> {
               onSubmit: (text) {
               }),
         ),
+        const Expanded(child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: SearchItemsGridList(),
+        ))
       ],
     );
   }
 }
+
+
+
+
