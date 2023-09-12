2d9c7dcbdf95c5ec51d502442b702f8d877f06bb
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-09-12 07:25:38 +0300

    adding the search_item_grid_list.dart using the DynamicHeightGridView

---
+++ lib/features/search/persentation/widgets/search_item_grid_list.dart
@@ -0,0 +1,17 @@
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_smart/features/search/persentation/widgets/product_item.dart';
+
+class SearchItemsGridList extends StatelessWidget {
+  const SearchItemsGridList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DynamicHeightGridView(
+        builder: (context, index) {
+          return const ProductItem();
+        },
+        itemCount: 20,
+        crossAxisCount: 2);
+  }
+}
