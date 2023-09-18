c6a2a41f6e7b8cc69e5b11364c86d1486e23a755
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-09-18 22:39:14 +0300

    finishing and adding the last arrival product to home_body.dart

--- lib/features/home/persentation/widgets/home_body.dart
+++ lib/features/home/persentation/widgets/home_body.dart
@@ -2,20 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:shop_smart/core/utils/global_widgets/custom_appbar.dart';
 import 'package:shop_smart/features/home/persentation/widgets/home_image_swiper.dart';
 
+import 'home_last_arrival.dart';
+
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppbar(title: 'ShopSmart', needIcon: false),
         SizedBox(height: 20,),
         HomeImageSwiper(),
+        Expanded(child: HomeLastArrival()),
 
       ],
     );
   }
 }
+
+
+
 
 
