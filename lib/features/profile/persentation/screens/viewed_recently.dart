27c1f0e133ce4c684338f643799961ca797930df
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-09-21 15:49:53 +0300

    adding viewed_recently.dart screen

---
+++ lib/features/profile/persentation/screens/viewed_recently.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:shop_smart/features/profile/persentation/widgets/viewed_recently_body.dart';
+
+class ViewedRecently extends StatelessWidget {
+  const ViewedRecently({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ViewedRecentlyBody(),
+    );
+  }
+}
+
+
---
+++ lib/features/profile/persentation/widgets/viewed_recently_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ViewedRecentlyBody extends StatelessWidget {
+  const ViewedRecentlyBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
