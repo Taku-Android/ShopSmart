423d032664cde9688dcd60fa8a46d9d8088f3135
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-09-28 22:37:24 +0300

    finishing the login_body.dart

--- lib/features/auth/persentation/widgets/login_body.dart
+++ lib/features/auth/persentation/widgets/login_body.dart
@@ -1,15 +1,62 @@
-
 import 'package:flutter/material.dart';
+import 'package:shop_smart/features/auth/persentation/widgets/login_form.dart';
+
+import '../../../../core/utils/styles.dart';
 
 class LoginBody extends StatelessWidget {
   const LoginBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              'ShopSmart',
+              textAlign: TextAlign.center,
+              style: Styles.headerText,
+            ),
+            // child: SizedBox(
+            //   child: Shimmer.fromColors(
+            //     period: const Duration(seconds: 2),
+            //     baseColor: Colors.purple,
+            //     highlightColor: Colors.red,
+            //     child: Text(
+            //       'ShopSmart',
+            //       textAlign: TextAlign.center,
+            //       style: Styles.headerText,
+            //     ),
+            //   ),
+            // ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Text(
+            'Welcome Back ',
+            textAlign: TextAlign.center,
+            style: Styles.titleText.copyWith(fontSize: 24),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            'Let`s get you logged in so you can start exploring',
+            textAlign: TextAlign.center,
+            style: Styles.subTitleText,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const LoginForm(),
+        ],
+      ),
     );
   }
 }
+
+
+
