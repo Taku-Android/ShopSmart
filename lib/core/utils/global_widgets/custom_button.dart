7de48f96acdb5d03b0becdcad9236d085b127ead
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-09-28 22:35:38 +0300

    modifying the custom_button.dart to be compatible with any screen

--- lib/core/utils/global_widgets/custom_button.dart
+++ lib/core/utils/global_widgets/custom_button.dart
@@ -5,31 +5,56 @@ import 'package:shop_smart/core/utils/styles.dart';
 import '../theme_cubit/theme_cubit.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.onPressed,
-    required this.title, required this.horizontalPadding, required this.textColor});
+  const CustomButton(
+      {super.key,
+      required this.onPressed,
+      required this.title,
+      this.horizontalPadding,
+      required this.textColor,
+      this.borderRadius, this.verticalPadding, this.textSize, this.image});
 
   final void Function()? onPressed;
-  final String title ;
-  final double horizontalPadding ;
-  final Color textColor ;
+  final String title;
+  final double? horizontalPadding;
+  final double? verticalPadding;
+  final double? borderRadius;
+  final double? textSize;
+  final String? image;
+  final Color textColor;
+
   @override
   Widget build(BuildContext context) {
     bool isDark = BlocProvider.of<ThemeCubit>(context).getIsDarkTheme;
     return ElevatedButton(
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>((isDark)?Colors.blue:Colors.blue[50]!), // Set the background color
+          backgroundColor: MaterialStateProperty.all<Color>(
+              (isDark) ? Colors.blue : Colors.blue[50]!),
+          // Set the background color
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0), // Set the corner radius
+              borderRadius: BorderRadius.circular(
+                  borderRadius ?? 20), // Set the corner radius
             ),
           ),
         ),
         onPressed: onPressed,
         child: Padding(
-          padding:  EdgeInsets.symmetric(horizontal: horizontalPadding , vertical: 8),
-          child: Text(title , style:  Styles.subTitleText.copyWith(
-      color: textColor
-    ),),
+          padding: EdgeInsets.symmetric(
+              horizontal: horizontalPadding ?? 2, vertical: verticalPadding ?? 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              (image != null)?Image.asset(image! , width: 20, height: 20, fit: BoxFit.fill,) : const SizedBox.shrink() ,
+              const SizedBox(width: 10,),
+              Text(
+                title,
+                style: Styles.subTitleText.copyWith(
+                    color: textColor ,
+                  fontSize: textSize
+                ),
+              ),
+            ],
+          ),
         ));
   }
 }
