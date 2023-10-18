275202897fd45786a7ba4297ffe90fec036fe92f
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-10-18 12:31:16 +0300

    finishing the vaildate on the image and form

--- lib/features/auth/persentation/widgets/register/register_form.dart
+++ lib/features/auth/persentation/widgets/register/register_form.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shop_smart/config/app_routes.dart';
 import 'package:shop_smart/config/dialog_utils.dart';
-
+import 'package:shop_smart/features/auth/persentation/widgets/register/register_form_design.dart';
 import '../../../../../core/utils/global_widgets/custom_button.dart';
-import '../../../../../core/utils/global_widgets/custom_text_field.dart';
 import '../../../../../core/utils/styles.dart';
 import 'image_picker_container.dart';
 
@@ -17,7 +15,7 @@ class RegisterForm extends StatefulWidget {
 }
 
 class _RegisterFormState extends State<RegisterForm> {
-  GlobalKey<FormState> registerKey = GlobalKey();
+  final registerKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
   TextEditingController nameController = TextEditingController();
@@ -31,7 +29,9 @@ class _RegisterFormState extends State<RegisterForm> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery
+        .of(context)
+        .size;
 
     return Column(
       children: [
@@ -41,110 +41,43 @@ class _RegisterFormState extends State<RegisterForm> {
             width: size.width / 3,
             height: size.height / 7,
             child:
-                ImagePickerContainer(pickedImage: pickedImage, function: () {
-                  DialogUtils.imagePickerDialog(context: context,
-                      onCameraPressed: () async {
-                          if(Navigator.canPop(context)){
-                            Navigator.pop(context);
-                          }
-                          await pickImageFromCamera();
-                      },
-                      onGalleryPressed: () async {
-                        if(Navigator.canPop(context)){
-                          Navigator.pop(context);
-                        }
-                        await pickImageFromGallery();
-                      },
-                      onRemovePressed: (){
-                        if(Navigator.canPop(context)){
-                          Navigator.pop(context);
-                        }
-                        setState(() {
-                          pickedImage = null ;
-                        });
-                      });
-                }),
+            ImagePickerContainer(pickedImage: pickedImage, function: () {
+              DialogUtils.imagePickerDialog(context: context,
+                  onCameraPressed: () async {
+                    if (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    }
+                    await pickImageFromCamera();
+                  },
+                  onGalleryPressed: () async {
+                    if (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    }
+                    await pickImageFromGallery();
+                  },
+                  onRemovePressed: () {
+                    if (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    }
+                    setState(() {
+                      pickedImage = null;
+                    });
+                  });
+            }),
           ),
         ),
-        CustomTextField(
-          hint: 'Name',
-          controller: nameController,
-          onSubmit: (value) {},
-          isPass: false,
-          onShowTap: () {},
-          icon: IconlyLight.user2,
-          isSecure: false,
-          validate: (submit) ? nameValidate() : true,
-          errMessage: 'Invalid Name..',
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        CustomTextField(
-          hint: 'Email',
-          controller: emailController,
-          onSubmit: (value) {},
-          isPass: false,
-          onShowTap: () {},
-          icon: IconlyLight.message,
-          isSecure: false,
-          validate: (submit) ? emailValidate() : true,
-          errMessage: 'Invalid Email..',
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        CustomTextField(
-          hint: 'Password',
-          controller: passController,
-          onSubmit: (value) {
-            if (!submit) {
-              setState(() {
-                submit = true;
-              });
-            }
-          },
-          isPass: true,
-          onShowTap: () {
-            if (isSecure) {
-              setState(() {
-                isSecure = false;
-              });
-            } else {
-              setState(() {
-                isSecure = true;
-              });
-            }
-          },
-          icon: IconlyLight.password,
-          isSecure: isSecure,
-          validate: (submit) ? passValidate() : true,
-          errMessage: 'Password must be at least 8 character..',
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        CustomTextField(
-          hint: 'Confirm Password',
-          controller: confPassController,
-          onSubmit: (value) {
-            if (!submit) {
-              setState(() {
-                submit = true;
-              });
-            }
-          },
-          isPass: true,
-          onShowTap: () {},
-          icon: IconlyLight.password,
-          isSecure: true,
-          validate: (submit) ? confValidate() : true,
-          errMessage: 'password don`t match..',
+        RegisterFormDesign(nameController: nameController,
+            emailController: emailController,
+            passController: passController,
+            confPassController: confPassController ,
+          registerKey: registerKey,
         ),
         Padding(
           padding: const EdgeInsets.only(top: 12.0, bottom: 24),
           child: CustomButton(
-              onPressed: () {},
+              onPressed: () {
+                register();
+              },
               title: 'Sign up',
               borderRadius: 8,
               verticalPadding: 12,
@@ -167,57 +100,34 @@ class _RegisterFormState extends State<RegisterForm> {
     );
   }
 
-  bool nameValidate() {
-    if (nameController.text.trim().isEmpty ||
-        nameController.text.trim().length < 6) {
-      return false;
+  register() {
+    if (registerKey.currentState?.validate() == false) {
+      return;
     }
-    return true;
-  }
+    if(pickedImage == null){
+      DialogUtils.showAlertDialog(context: context, onPressed: (){}, title: 'Image is required', isCancel: false);
+    }
 
-  bool emailValidate() {
-    if (emailController.text.trim().isEmpty) {
-      return false;
-    }
-    return true;
-  }
 
-  bool passValidate() {
-    if (passController.text.trim().isEmpty) {
-      return false;
-    } else if (passController.text.trim().length < 8) {
-      return false;
-    }
-    return true;
-  }
-
-  bool confValidate() {
-    if (confPassController.text.trim().isEmpty) {
-      return false;
-    } else if (confPassController.text != passController.text) {
-      return false;
-    }
-    return true;
   }
 
 
   Future<void> pickImageFromCamera() async {
     final ImagePicker picker = ImagePicker();
-    pickedImage =  await picker.pickImage(source: ImageSource.camera);
+    pickedImage = await picker.pickImage(source: ImageSource.camera);
 
-    setState(()  {
-    });
-
+    setState(() {});
   }
 
   Future<void> pickImageFromGallery() async {
     final ImagePicker picker = ImagePicker();
-    pickedImage =  await picker.pickImage(source: ImageSource.gallery);
+    pickedImage = await picker.pickImage(source: ImageSource.gallery);
 
-    setState(()  {
-    });
-
+    setState(() {});
   }
 
 
 }
+
+
+
