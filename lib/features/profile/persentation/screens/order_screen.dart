faf5f7b8284994f4bcfdc120347f06d23f159ade
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-10-18 13:18:31 +0300

    adding the order_screen.dart

---
+++ lib/features/profile/persentation/screens/order_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../widgets/order_body.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: OrderBody()),
+    );
+  }
+}
